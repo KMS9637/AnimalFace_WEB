5e5d4522fd38c60f405540cd2fd6b28c657c1868
KMS9637 <kmsa0202@gmail.com>
2024-08-10 13:23:46 +0900

    first commit

---
+++ src/main/java/com/project/animalface_web/AnimalFaceWebApplication.java
@@ -0,0 +1,13 @@
+package com.project.animalface_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFaceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalFaceWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/animalface_web/AnimalFaceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.animalface_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFaceWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
