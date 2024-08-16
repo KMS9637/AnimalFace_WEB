1fd09e110350a3c2fbadd2ce69e202f7b2734f9d
kimsoyoung <ggomag3243@gmail.com>
2024-08-16 18:47:53 +0900

    notice 백 작업 중

--- src/main/java/com/project/animalface_web/service/ksyserviece/NoticeServiceImpl.java
+++ src/main/java/com/project/animalface_web/service/ksyserviece/NoticeServiceImpl.java
@@ -25,6 +25,7 @@ public class NoticeServiceImpl implements NoticeService {
         Notice notice = dtoToEntity(noticeDTO);
         Long noticeNo = noticeRepository.save(notice).getNoticeNo();
         return noticeNo;
+
     }
 
     @Override
