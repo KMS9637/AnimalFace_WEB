762d8200f11d6f495f8a1fdc5a79a744f8f11291
kimsoyoung <ggomag3243@gmail.com>
2024-08-16 18:21:07 +0900

    @Builder 추가

--- src/main/java/com/project/animalface_web/domain/Notice.java
+++ src/main/java/com/project/animalface_web/domain/Notice.java
@@ -2,6 +2,7 @@ package com.project.animalface_web.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Notice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
