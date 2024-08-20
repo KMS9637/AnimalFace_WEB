bb6cc4ea21cdd4a9e9e3356ddad41e5bf852048f
KODOKYUN <hwasungnj3@gmail.com>
2024-08-20 17:16:35 +0900

    240820 BaseEntity 작업, DTO 작업중2

--- src/main/java/com/project/animalface_web/dto/kdkdto/NoticeDTO.java
+++ src/main/java/com/project/animalface_web/dto/kdkdto/NoticeDTO.java
@@ -1,13 +1,10 @@
 package com.project.animalface_web.dto.kdkdto;
 
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -16,8 +13,8 @@ import java.util.Date;
 @NoArgsConstructor
 
 public class NoticeDTO {
-
+    private Long noticeNo;
     private String noticeName;
     private String noticeContents;
-    private LocalDateTime regDate;
+    private Date date;
 }
