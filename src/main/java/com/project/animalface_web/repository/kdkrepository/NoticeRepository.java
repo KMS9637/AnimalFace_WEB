6d27324a10060ba6c443417178a60c87921dbc2e
KODOKYUN <hwasungnj3@gmail.com>
2024-08-22 18:37:30 +0900

    240821 CreateGame 백 작업중3

--- src/main/java/com/project/animalface_web/repository/kdkrepository/NoticeRepository.java
+++ src/main/java/com/project/animalface_web/repository/kdkrepository/NoticeRepository.java
@@ -4,4 +4,5 @@ import com.project.animalface_web.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
 }
