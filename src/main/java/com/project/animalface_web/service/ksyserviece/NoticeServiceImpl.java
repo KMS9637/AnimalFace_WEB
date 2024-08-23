15f33ed3a64c8734a335656dcac2aaeae8be8d4a
kimsoyoung <ggomag3243@gmail.com>
2024-08-23 19:20:18 +0900

    rest 서버 연결 완

--- src/main/java/com/project/animalface_web/service/ksyserviece/NoticeServiceImpl.java
+++ src/main/java/com/project/animalface_web/service/ksyserviece/NoticeServiceImpl.java
@@ -1,8 +1,8 @@
 package com.project.animalface_web.service.ksyserviece;
 
 import com.project.animalface_web.domain.Notice;
-import com.project.animalface_web.dto.ksydto.NoticeDTO;
-import com.project.animalface_web.repository.ksyrepository.NoticeRepository;
+import com.project.animalface_web.dto.NoticeDTO;
+import com.project.animalface_web.repository.NoticeRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -10,7 +10,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -40,15 +39,34 @@ public class NoticeServiceImpl implements NoticeService {
     }
 
     @Override
-    public void update(NoticeDTO noticeDTO) {
-        Optional<Notice> result = noticeRepository.findById(noticeDTO.getNoticeNo());
-        Notice notice = result.orElseThrow();
-        notice.updateNameAndContent(noticeDTO.getNoticeName(), noticeDTO.getNoticeContents());
-        noticeRepository.save(notice);
+    public NoticeDTO update(Long noticeNo, NoticeDTO noticeDTO) {
+        Optional<Notice> optionalNotice = noticeRepository.findById(noticeNo);
+        if (optionalNotice.isPresent()) {
+            Notice notice = optionalNotice.get();
+            notice.setNoticeName(noticeDTO.getNoticeName());
+            notice.setNoticeContents(noticeDTO.getNoticeContents());
+            notice.setDate(noticeDTO.getDate());
+
+            Notice updatedNotice = noticeRepository.save(notice);
+            return entityToDto(updatedNotice);
+        }
+        return null;  // or throw an exception
     }
 
     @Override
-    public void delete(Long noticeNo) {
+    public boolean delete(Long noticeNo) {
         noticeRepository.deleteById(noticeNo);
+        return false;
+    }
+
+    @Override
+    public NoticeDTO save(NoticeDTO noticeDTO) {
+        Notice notice = new Notice();
+        notice.setNoticeName(noticeDTO.getNoticeName());
+        notice.setNoticeContents(noticeDTO.getNoticeContents());
+        notice.setDate(noticeDTO.getDate());
+
+        Notice savedNotice = noticeRepository.save(notice);
+        return entityToDto(savedNotice);
     }
 }
