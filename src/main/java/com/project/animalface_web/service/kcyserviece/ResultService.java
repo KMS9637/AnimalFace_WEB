4ed7c490ce2f1ea4f76b78cbac6f854cfdda6bcd
cherrye0ng <dlqndl5297@gmail.com>
2024-08-26 17:29:22 +0900

    결과화면 완료

---
+++ src/main/java/com/project/animalface_web/service/kcyserviece/ResultService.java
@@ -0,0 +1,11 @@
+package com.project.animalface_web.service.kcyserviece;
+import com.project.animalface_web.repository.ResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public interface ResultService {
+
+
+}
---
+++ src/main/java/com/project/animalface_web/service/kcyserviece/ResultServiceImpl.java
@@ -0,0 +1,10 @@
+package com.project.animalface_web.service.kcyserviece;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResultServiceImpl implements ResultService {
+
+
+}
