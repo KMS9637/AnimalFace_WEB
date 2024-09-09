b70133135c7cb263373606eef77a3324ffeca282
KODOKYUN <hwasungnj3@gmail.com>
2024-09-09 12:27:03 +0900

    240909 AnimalFace 안드로이드와 연결하기 위한 수정

--- src/main/java/com/project/animalface_web/controller/kmscontroller/MemberRestController.java
+++ src/main/java/com/project/animalface_web/controller/kmscontroller/MemberRestController.java
@@ -4,12 +4,8 @@ import com.project.animalface_web.domain.Member;
 import com.project.animalface_web.service.MemberService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
