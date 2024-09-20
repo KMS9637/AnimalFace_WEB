739d20b1dd6e7f6a8f8d0281261bc6ec5c971f43
Kimminsu <kmsa0202@gmail.com>
2024-09-20 10:41:52 +0900

    240920 합치기 준비2

--- src/main/java/com/project/animalface_web/domain/game/Game.java
+++ src/main/java/com/project/animalface_web/domain/game/Game.java
@@ -25,4 +25,3 @@ public class Game extends BaseEntity {
 }
 
 
-
--- src/main/java/com/project/animalface_web/domain/quiz/Quiz.java
+++ src/main/java/com/project/animalface_web/domain/quiz/Quiz.java
@@ -1,4 +1,26 @@
 package com.project.animalface_web.domain.quiz;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quizNo;
+    private String quizName;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<QuizQuestion> questions;
+
+
 }
